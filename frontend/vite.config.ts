873ab27f88f67fd853e3bd5f666295a38a6e95d2
suraj-bvc <suraj@birdvision.in>
2026-08-06 13:13:50 +0530

    moved frontend code to folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+
+  vite: {
+    server: {
+      allowedHosts: [
+        "jonnie-burry-nonsymbolically.ngrok-free.dev",
+      ],
+    },
+  },
 });
